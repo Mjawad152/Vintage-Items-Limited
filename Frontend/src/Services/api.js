5b57a1bb0324d7cff2112141e03d3b03df4f1f84
Muhammad Jawad Iqbal <muhammadjawadiqbal5@gmail.com>
2023-12-16 23:18:05 +0500

    Update api.js

--- Frontend/src/Services/api.js
+++ Frontend/src/Services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const baseURL = 'https://vintage-items-limited-frontend.vercel.app';
+const baseURL = 'http://localhost:5000';
 
 const api = axios.create({
   baseURL: baseURL,
